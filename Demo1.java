1c2e0afb88fed46f9b4aa35015ee9eb3913d4dd8
Vyshnavi Nagunuri <nagunurivyshnavi@gmail.com>
2021-11-01 15:59:36 +0530

    Added Lines

--- Demo1.java
+++ Demo1.java
@@ -2,5 +2,9 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
         System.out.print("Added Java");
+         System.out.print("Added Line 2");
+         System.out.print("Added Line 3");
+        
+        
     }  
 } 
